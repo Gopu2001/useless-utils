ed302995884c6426e0f6f53629473b4e546eca9f
Anmol Kapoor <anmolhome0227@gmail.com>
2023-03-29 23:49:03 -0700

    ak - adding double comprehension/loop + tests

--- python/comprehensions.py
+++ python/comprehensions.py
@@ -39,6 +39,41 @@ def simple_comprehension(arr: List[str]) -> List[str]:
     return result
 
 
+@timeit
+def double_loop(img: List[List[int]]) -> List[int]:
+    """
+    Perform a double for loop and flatten a 2D image
+    """
+    result = []
+
+    for y in img:
+        for x in y:
+            result.append(x)
+
+    return result
+
+
+@timeit
+def double_comprehension(img: List[List[int]]) -> List[int]:
+    """
+    Perform a double list comprehension and flatten a 2D image
+    """
+    result = [x for y in img for x in y]
+
+    return result
+
+
 if __name__ == "__main__":
-    simple_comprehension(list("abcghimnostuyz"))
-    simple_loop(list("abcghimnostuyz"))
+    string_test = ["a", "b", "c", "g", "h", "i", "m", "n", "o", "s", "t", "u", "y", "z"]
+    simple_comprehension(string_test)
+    simple_loop(string_test)
+    print()
+
+    img_test = [
+        [0,1,2,1],
+        [2,3,4,3],
+        [4,5,6,3],
+        [1,0,5,2]
+    ]
+    double_comprehension(img_test)
+    double_loop(img_test)
--- python/tests/test_comprehensions.py
+++ python/tests/test_comprehensions.py
@@ -3,7 +3,10 @@
 import os
 os.environ["PYTHON_USELESS_UTILS_TIMER"] = "FALSE"
 
-from comprehensions import simple_comprehension, simple_loop
+from comprehensions import (
+    simple_comprehension, simple_loop,
+    double_comprehension, double_loop,
+)
 
 def test_simple():
     inpt = ["a", "g", "c", "m", "d", "z"]
@@ -15,3 +18,13 @@ def test_simple():
     assert otpt_cmp == otpt_lp
     assert expected_output == otpt_cmp
 
+
+def test_double():
+    inpt = [[0, 1], [2, 3, 4, 3, 6], [4, 6, 3], [1, 0, 5]]
+    expected_output = [0, 1, 2, 3, 4, 3, 6, 4, 6, 3, 1, 0, 5]
+
+    otpt_cmp = double_comprehension(inpt)
+    otpt_lp = double_loop(inpt)
+
+    assert otpt_cmp == otpt_lp
+    assert expected_output == otpt_cmp
